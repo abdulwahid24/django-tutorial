c1846ec9541034bbfe9fd0a3177e1d4a692012ba
Abdul Wahid <abdulwahid24@gmail.com>
2013-06-19 10:55:44 +0530

    removed commented lines to last commit.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -6,10 +6,6 @@ from django.views import generic
 
 from polls.models import Poll, Choice
 
-# def index(request):
-#     latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-#     context = {'latest_poll_list':latest_poll_list}
-#     return render(request, 'polls/index.html', context)
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_poll_list'
@@ -17,26 +13,14 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         return Poll.objects.order_by('-pub_date')[:5]
 
-# def detail(request,poll_id):
-#     poll = get_object_or_404(Poll, pk=poll_id)
-#     return render(request, 'polls/details.html', {'poll':poll})
-
 class DetailView(generic.DetailView):
     model = Poll
     template_name = 'polls/details.html'
     
-
-# def result(request,poll_id):
-#     poll = get_object_or_404(Poll,pk=poll_id)
-#     return render(request, 'polls/results.html', {'poll':poll})
-
 class ResultView(generic.DetailView):
     model = Poll
     template_name = 'polls/results.html'
     
-    
-
-
 def vote(request, poll_id):
     poll = get_object_or_404(Poll, pk=poll_id)
     try:
